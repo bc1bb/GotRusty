6b0d58584838576b40688354003d9a76af5065a6
jusdepatate <jusdepatate@protonmail.com>
2022-10-25 19:56:43 +0200

    idiot proof on `Server::parse_addr`

--- src/gr_structs.rs
+++ src/gr_structs.rs
@@ -45,6 +45,26 @@ impl Server {
         // Split str argument into Vec<&str>
         let split_addr: Vec<&str> = addr.split(".").collect();
 
+        // Stupido checks
+        let mut panic = false;
+        if split_addr.clone().len() > 4 {
+            panic = true;
+        };
+        for i in split_addr.clone() {
+            let j = i.parse::<u32>();
+
+            if j.is_err() {
+                panic = true
+            }
+            if j.unwrap() > 254 {
+                panic = true;
+            };
+        }
+
+        if panic {
+            panic!("Given IP address for server seems invalid.");
+        };
+
         // Use splits to build IpAddr::V4
         return IpAddr::V4(Ipv4Addr::new(
             split_addr[0].to_string().parse().unwrap(),
@@ -52,8 +72,6 @@ impl Server {
             split_addr[2].to_string().parse().unwrap(),
             split_addr[3].to_string().parse().unwrap(),
         ));
-
-        // TODO: idiot-proof
     }
 
     pub fn get_addr(self) -> IpAddr {
